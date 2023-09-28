441f59d188d227390be34c33bd7558f320a14e2a
lehieuhuy <59508262+lehieuhuy@users.noreply.github.com>
2023-09-28 18:10:46 +0700

    Add files via upload

---
+++ Windows_setup (1).ps1
@@ -0,0 +1,72 @@
+# Name is Important =))
+write-host " #     # #     # #     #    #       ####### " -foregroundcolor "Red"
+write-host " #     # #     #  #   #     #       #       " -foregroundcolor "Red"
+write-host " #     # #     #   # #      #       #       " -foregroundcolor "White"
+write-host " ####### #     #    #       #       #####   " -foregroundcolor "White"
+write-host " #     # #     #    #       #       #       " -foregroundcolor "Green"
+write-host " #     # #     #    #       #       #       " -foregroundcolor "Green"
+write-host " #     #  #####     #       ####### ####### " -foregroundcolor "Yellow"
+                                  
+
+echo "  This script is made by Cyber Zest"
+# Set Wget Progress to Silent, Becuase it slows down Downloading by 50x
+echo "Setting Wget Progress to Silent, Becuase it slows down Downloading by 50x`n"
+$ProgressPreference = 'SilentlyContinue'
+
+# Check JDK-18 or 19 Availability or Download JDK-18 or 19
+$jdk19 = Get-WmiObject -Class Win32_Product -filter "Vendor='Oracle Corporation'" |where Caption -clike "Java(TM) SE Development Kit 19*"
+if (!($jdk19)){
+    echo "`t`tDownnloading Java JDK-19 ...."
+    wget "https://download.oracle.com/java/19/archive/jdk-19.0.2_windows-x64_bin.msi" -O jdk-19.exe  
+    echo "`n`t`tJDK-19 Downloaded, lets start the Installation process"
+    start -wait jdk-19.exe
+    rm jdk-19.exe
+}else{
+    echo "Required JDK-19 is Installed"
+    $jdk19
+}
+
+# Check JRE-8 Availability or Download JRE-8
+$jre8 = Get-WmiObject -Class Win32_Product -filter "Vendor='Oracle Corporation'" |where Caption -clike "Java 8 Update *"
+if (!($jre8)){
+    echo "`n`t`tDownloading Java JRE ...."
+    wget "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=246474_2dee051a5d0647d5be72a7c0abff270e" -O jre-8.exe
+    echo "`n`t`tJRE-8 Downloaded, lets start the Installation process"
+    start -wait jre-8.exe
+    rm jre-8.exe
+}else{
+    echo "`n`nRequired JRE-8 is Installed`n"
+    $jre8
+}
+
+# Downloading Burp Suite Professional
+iwr -Uri https://portswigger-cdn.net/burp/releases/download?product=pro&version=2023.9.2&type=Jar -Outfile Burp-Suite-Pro.jar
+
+
+# Creating Burp.bat file with command for execution
+if (Test-Path burp.bat) {rm burp.bat}
+$path = "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:`"$pwd\loader.jar`" -noverify -jar `"$pwd\burpsuite_pro_v$version.jar`""
+$path | add-content -path Burp.bat
+echo "`nBurp.bat file is created"
+
+
+# Creating Burp-Suite-Pro.vbs File for background execution
+if (Test-Path Burp-Suite-Pro.vbs) {
+   Remove-Item Burp-Suite-Pro.vbs}
+echo "Set WshShell = CreateObject(`"WScript.Shell`")" > Burp-Suite-Pro.vbs
+add-content Burp-Suite-Pro.vbs "WshShell.Run chr(34) & `"$pwd\Burp.bat`" & Chr(34), 0"
+add-content Burp-Suite-Pro.vbs "Set WshShell = Nothing"
+echo "`nBurp-Suite-Pro.vbs file is created."
+
+# Remove Additional files
+rm Linux_setup.sh
+del -Recurse -Force .\.github\
+
+
+# Lets Activate Burp Suite Professional with keygenerator and Keyloader
+echo "Reloading Environment Variables ...."
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+echo "`n`nStarting Keygenerator ...."
+start-process java.exe -argumentlist "-jar loader.jar"
+echo "`n`nStarting Burp Suite Professional"
+java --add-opens=java.desktop/javax.swing=ALL-UNNAMED--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:"loader.jar" -noverify -jar "Burp-Suite-Pro.jar"
